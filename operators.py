5c847b70a786b702a55647c3f230872b62b33762
aharini5745-droid <a.harini5745@gmail.com>
2026-05-21 16:20:16 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,37 @@
+a=10
+b=15
+c=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%c)
+print(a**c)
+
+
+a=500
+b=250
+print(a==b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(b>=a)
+print(a!=b)
+
+a=5
+b=10
+c=23
+d=35
+print (a>5 and b<8 and d>25)#false f  t 
+print (a>5 and c<23)
+print(a>1 or b>8)
+print (c>20 or a<4)
+print(not(c>20))
+
+
+
+a=2
+print(a<7 and a<10,a==2 )# t t 
+print(a>10 or a==5,a<5)# f t
+print(not a>3,a<6 )#t  t
